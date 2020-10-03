a582220ba649568d599d2a5ac7fef16c596b6579
alfianyusufabdullah <alfianyabdullah@gmail.com>
2020-10-03 16:17:34 +0700

    added new variable for title column

--- src/model/Discussions.kt
+++ src/model/Discussions.kt
@@ -3,6 +3,7 @@ package alfianyusufabdullah.discussionservice.model
 data class Discussions(
     val id: Int = 0,
     val parentId: Int = 0,
+    val title: String = "",
     val name: String = "",
     val comment: String = "",
     val createdAt: Long = 0,
