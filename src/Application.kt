f1c4e09c23cdecef4300812b0e1177525214df4b
alfianyusufabdullah <alfianyabdullah@gmail.com>
2020-10-04 12:47:03 +0700

    added new transaction to insert dummy data

--- src/Application.kt
+++ src/Application.kt
@@ -1,13 +1,17 @@
 package alfianyusufabdullah.discussionservice
 
+import alfianyusufabdullah.discussionservice.data.Dummy
 import alfianyusufabdullah.discussionservice.database.DatabaseConfiguration
 import alfianyusufabdullah.discussionservice.repository.DatabaseRepository
 import alfianyusufabdullah.discussionservice.database.DiscussionTable
+import alfianyusufabdullah.discussionservice.database.dbQuery
+import alfianyusufabdullah.discussionservice.model.Discussions
 import alfianyusufabdullah.discussionservice.rest.widget
 import io.ktor.application.*
 import io.ktor.routing.*
 import io.ktor.gson.*
 import io.ktor.features.*
+import kotlinx.coroutines.launch
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils.create
 import org.jetbrains.exposed.sql.transactions.transaction
@@ -19,6 +23,27 @@ fun Application.module() {
     Database.connect(DatabaseConfiguration.hikariConfigDataSource())
     transaction {
         create(DiscussionTable)
+
+        launch {
+            val databaseRepository = DatabaseRepository()
+            val discussions = Dummy.discussions
+            discussions.forEachIndexed { index, value ->
+                println("Insert new ${value}, data size ${discussions.size}")
+
+                when (value.keys.elementAt(0)) {
+                    "discussion" -> {
+                        databaseRepository.publishNewDiscussion(value.values.elementAt(0))
+                    }
+                    "comment" -> {
+                        databaseRepository.publishNewComment(value.values.elementAt(0))
+                    }
+                }
+            }
+
+            discussions.forEach {
+
+            }
+        }
     }
 
     install(ContentNegotiation) {
