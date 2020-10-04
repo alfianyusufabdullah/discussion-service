0e53e5c2998fdc633a6596e8c17520294e632968
alfianyusufabdullah <alfianyabdullah@gmail.com>
2020-10-04 11:55:49 +0700

    changes some column type

--- src/database/DiscussionTable.kt
+++ src/database/DiscussionTable.kt
@@ -3,10 +3,10 @@ package alfianyusufabdullah.discussionservice.database
 import org.jetbrains.exposed.sql.Table
 
 object DiscussionTable : Table() {
-    val id = integer("id").primaryKey().autoIncrement()
-    val parent_id = integer("parent_id")
+    val id = varchar("id", 255).primaryKey()
+    val parent_id = varchar("parent_id", 255)
     val title = varchar("title", 255).nullable()
     val name = varchar("name", 255)
-    val comment = varchar("comment", 255)
+    val comment = text("comment")
     val created_at = long("created_at")
 }
